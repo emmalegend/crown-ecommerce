f05542728c9e20b0d8de3363640a8d84529ec3e0
Emmanuel <emmanuel.effiong361@gmail.com>
2020-02-08 11:03:21 +0100

    implemented friebase and created the signup component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './Firebase/firebase.util';
 
 class App extends React.Component {
   constructor (){
@@ -16,22 +16,36 @@ class App extends React.Component {
     }
   }
 
-  unSubscribedFromAuth = null
+  unSubscribeFromAuth = null
 
   componentDidMount (){
-    this.unSubscribedFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({ currentUser: user});
+    this.unSubscribeFromAuth = auth.onAuthStateChanged( async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      else{
+        this.setState({currentUser: userAuth})
+      }
     });
   }
 
-  componentWillMount(){
-    this.unSubscribedFromAuth();
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
   }
 
   render () {
     return (
       <div>
-        <Header />
+        <Header currentUser ={this.state.currentUser}/>
         <Switch>
           <Route exact path ='/' component={HomePage} />
           <Route path ='/shop' component={ShopPage} />
