ae5d5d126e862fbae0197248f4dd50d87b93f201
Emmanuel <emmanuel.effiong361@gmail.com>
2020-02-10 06:25:19 +0100

    added redux lib to help us manage our state, then also added a redirect to homepage if we are signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {auth} from '../../Firebase/firebase.util';
 import './header.styles.scss';
@@ -31,5 +32,8 @@ const Header = ({currentUser}) => (
         </div>
     </div>
 )
+const mapStateToProps  = state => ({
+    currentUser: state.user.currentUser
+})
 
-export default Header
+export default connect(mapStateToProps)(Header)
